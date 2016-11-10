9016effa87794120d9b0dbce3f61f1e933b03bcf
filipetm <filipetm@gmail.com>
2016-11-09 21:38:18 -0300

    exportando função e retirando restrição de só atualizar a cada dois dias

--- R/getData.R
+++ R/getData.R
@@ -1,14 +1,16 @@
 #' @title get data
 #' @description Funcao para recuperar dados do tesouro direto que estao na pasta data com o nome 'precotaxatesourodireto.csv'
 #' @author Rodrigo Almeida
+#' @export
 getData <- function() {
 
   if (!exists("dataCarregamentoTesouroDireto")) {
+
     dataCarregamentoTesouroDireto <<- Sys.Date()
   }
 
-  # A cada dois dias, atualizar os dados do tesouro direto
-  if ((!exists("precotaxatesourodireto")) || (difftime(Sys.Date(),dataCarregamentoTesouroDireto) > 2)) {
+  if ( !exists("precotaxatesourodireto") ) {
+
     dataCarregamentoTesouroDireto <<- Sys.Date()
     precotaxatesourodireto <- read.csv2("data/precotaxatesourodireto.csv", sep = ",")
   }
