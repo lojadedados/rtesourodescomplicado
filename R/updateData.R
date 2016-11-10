b2f7704071ff994f0f7d3be8b97c3b9eb7230753
filipetm <filipetm@gmail.com>
2016-11-09 21:37:20 -0300

    retornando a data/hora da última atualização

--- R/updateData.R
+++ R/updateData.R
@@ -3,8 +3,10 @@
 #' @author Rodrigo Almeida - rodrigo@lojadedados.com.br
 #' @export
 updateData <- function() {
+
   url <- "https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv"
   s <- read.csv2(url)
-  write.csv(x = s, file = "data/precotaxatesourodireto.csv", sep = ",")
-  # TODO: Criar arquivo com a data da ultima atualizacao
+  write.csv(x = s, file = "data/precotaxatesourodireto.csv")
+
+  format(Sys.Date(), "%d/%m/%Y %H:%M")
 }
